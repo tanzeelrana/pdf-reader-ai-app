9e3de7c08a3217faa485238587ad8a52301ce65e
tanzeel <tanzeel_milton@tanzeelmilton.home>
2024-12-11 16:08:15 +0000

    fix bugs

--- app.py
+++ app.py
@@ -35,9 +35,9 @@ EMBEDDINGS = HuggingFaceBgeEmbeddings(
 )
 
 LLM = HuggingFacePipeline.from_model_id(
-    model_id="gpt2-medium",
+    model_id="gpt2-large",
     task="text-generation",
-    pipeline_kwargs={"temperature": 0.3, "max_new_tokens": 100, "do_sample": True}
+    pipeline_kwargs={"temperature": 0.6, "max_new_tokens": 100, "do_sample": True}
 )
 
 def split_documents(file_path):
